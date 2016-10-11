947767f374397e3f46a3a01041e196e6b1ea7fd3
Nick Booher <njbooher@gmail.com>
2016-10-11 15:44:54 -0500

    Move skip link before alphabet soup

--- templates/section--header.tpl.php
+++ templates/section--header.tpl.php
@@ -1,5 +1,8 @@
 <header<?php print $attributes; ?>>
   <div id="section-header-container">
+    <div id="skip-link">
+      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
+    </div>
     <?php if ($show_blackbar): ?>
       <div class="pwrapper">
         <div class="pwrapper-wrapper">
@@ -44,9 +47,6 @@
         </div>
       </div>
     <?php endif; ?>
-    <div id="skip-link">
-      <a href="#main-content" class="element-invisible element-focusable"><?php print t('Skip to main content'); ?></a>
-    </div>
     <?php print $content; ?>
   </div>
 </header>
