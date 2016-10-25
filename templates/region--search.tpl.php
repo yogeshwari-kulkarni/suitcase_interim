66499862ef7e2e68e994b274d99c2ed8e7f591b2
Angela McMahon <angelam@iastate.edu>
2016-10-25 16:40:43 -0500

    LUGG-841 Moved around template logic a bit so padding would work according to how many site name levels existed

--- templates/region--search.tpl.php
+++ templates/region--search.tpl.php
@@ -1,14 +1,11 @@
 <div<?php print $attributes; ?>>
   <div<?php print $content_attributes; ?>>
+    <?php if ($content): ?>
     <div class="clearfix">
       <?php $site_name_level_2_class = ($suitcase_interim_config_header_type < 3) ? ' has-site-name-level-2' : ''; ?>
       <?php $site_name_level_3_class = ($suitcase_interim_config_header_type == 1 || $suitcase_interim_config_header_type == 3 || $suitcase_interim_config_header_type == 4) ? ' has-site-name-level-3' : ''; ?>
-      <div class="pull-right margin-left-10 pos-relative search-form-container<?php print $site_name_level_2_class . $site_name_level_3_class; ?>"></div>
+        <div class="pull-right margin-left-10 pos-relative search-form-container<?php print $site_name_level_2_class . $site_name_level_3_class; ?>"><?php print $content; ?></div>
     </div>
-    <?php if ($content): ?>
-      <div class="clearfix">
-        <div class="pull-right"><?php print $content; ?></div>
-      </div>
     <?php endif; ?>
   </div>
 </div>
