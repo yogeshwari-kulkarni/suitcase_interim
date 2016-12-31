8e432516a33a11daa5e719465ca371bffa7d234c
Nick Booher <njbooher@gmail.com>
2016-12-31 16:17:44 -0600

    Create default footer blocks

---
+++ templates/default_region_content_blocks/footer_fourth.tpl.php
@@ -0,0 +1,8 @@
+<ul class="menu">
+  <li class="first leaf">
+    <a href="<?php print $GLOBALS['base_url']; ?>/site-index"><i class="fa fa-list-ul" aria-hidden="true"></i>Site Index</a>
+  </li>
+  <li class="last leaf">
+    <a href="<?php print $GLOBALS['base_url']; ?>/sitemap" title="Display a site map with RSS feeds."><i class="fa fa-sitemap" aria-hidden="true"></i>Site map</a>
+  </li>
+</ul>
