c1a5169da19f0baaf2fb93fdfb6f4159ac9fe271
Carney, Sarah A <sacarney@iastate.edu>
2017-03-07 10:48:38 -0600

    INTERIM-12 Add jQuery
    Added jQuery to find all external links and add a class for styling.

---
+++ js/suitcase_external_links.js
@@ -0,0 +1,15 @@
+/**
+ * Function for adding a class to external links
+ * so they can be styled.
+ */
+
+(function ($) {
+
+	$(document).ready(function() {
+
+		$('a').filter(function() {
+			return this.hostname && this.hostname !== location.hostname;
+		}).addClass('external');
+	});
+
+})(jQuery);
