ebdb788054cd38b9e320f944568d13f93a551404
Nick Booher <njbooher@gmail.com>
2017-03-20 11:49:09 -0500

    INTERIM-16 Add button style to the Styles dropdown

---
+++ ckeditor.styles.js
@@ -0,0 +1,14 @@
+/*
+ * This file is used/requested by the 'Styles' button.
+ */
+if (typeof(CKEDITOR) !== 'undefined') {
+  CKEDITOR.addStylesSet('drupal', [
+    {
+      name: 'Button',
+      element: 'a',
+      attributes: {
+        'class': 'button',
+      }
+    },
+  ]);
+}
